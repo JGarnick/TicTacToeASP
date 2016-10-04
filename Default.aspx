36696116097bc162443927839e0094c68195e6c7
JGarnick <garnick.josh@gmail.com>
2016-10-04 10:52:17 -0700

    Tic Tac Toe asp.net

---
+++ Default.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>My Tic Tac Toe</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 170px;
+        }
+        .auto-style2 {
+            width: 170px;
+            height: 161px;
+        }
+        .auto-style3 {
+            height: 161px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <p style="width: 174px; margin-left: 160px">
+            <asp:Image ID="TitleImage" runat="server" ImageUrl="~/Images/ttt.jpg" Width="153px" />
+        </p>
+        <asp:Table runat="server" style="width: 38%; height: 481px;">
+            <asp:TableRow>
+                <asp:TableCell style="border: solid red 1px;" CssClass="auto-style1">
+                    <asp:ImageButton ID="Image1" runat="server" BorderColor="Red" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image1_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+                <asp:TableCell style="border: solid red 1px;" CssClass="auto-style2">
+                    <asp:ImageButton ID="Image2" runat="server" BorderColor="Red" Height="152px" ImageUrl="~/Images/blank.jpg" Width="164px" OnClick="Image2_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+                <asp:TableCell style="border: solid red 1px;" CssClass="auto-style3">
+                    <asp:ImageButton ID="Image3" runat="server" BorderColor="Red" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image3_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell style="border:solid red 1px;" CssClass="auto-style1">
+                    <asp:ImageButton ID="Image4" runat="server" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image4_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+                <asp:TableCell style="border:solid red 1px;" CssClass="auto-style2">
+                    <asp:ImageButton ID="Image5" runat="server" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image5_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+                <asp:TableCell style="border:solid red 1px;" CssClass="auto-style3">
+                    <asp:ImageButton ID="Image6" runat="server" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image6_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell style="border:solid red 1px;" CssClass="auto-style1">
+                    <asp:ImageButton ID="Image7" runat="server" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image7_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+                <asp:TableCell style="border:solid red 1px;" CssClass="auto-style2">
+                    <asp:ImageButton ID="Image8" runat="server" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image8_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+                <asp:TableCell style="border:solid red 1px;" CssClass="auto-style3">
+                    <asp:ImageButton ID="Image9" runat="server" Height="152px" ImageUrl="~/Images/blank.jpg" Width="163px" OnClick="Image9_Click" ViewStateMode="Enabled" />
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <p style="width: 166px; margin-left: 160px;">
+            <asp:Label ID="results" runat="server" BorderColor="Black"></asp:Label>
+        </p>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //TODO: add an instance variable for the board object called board
+    private Board board = new Board();
+
+    //TODO: assumes that each square is an ImageButton with id of Image# (1 - 9)
+    //if you use a label or a textbox you'll have to change the datatype here
+    private ImageButton GetSquare(int row, int column)
+    {
+        int number = row * 3 + column + 1;
+        return (ImageButton)this.FindControl("Image" + number.ToString());
+    }
+
+    //TODO: assumes that each square is an ImageButton with id of Image# (1 - 9)
+    //if you use a label or a textbox you'll have to change the datatype here
+    private void GetRowAndColumn(ImageButton i, out int row, out int column)
+    {
+        int position = int.Parse(i.ID.Substring(5));
+        row = (position - 1) / 3;
+        column = (position - 1) % 3;
+    }
+
+    private void HighlightColumn(int col)
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            //TODO: change this
+            ImageButton square = GetSquare(row, col);
+            square.ImageUrl = "Images/Red" + board.GetSquare(row, col) + ".jpg";
+        }
+    }
+
+    private void HighlightDiagonal1()
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            //TODO: change this
+            ImageButton square = GetSquare(row, row);
+            square.ImageUrl = "Images/Red" + board.GetSquare(row, row) + ".jpg";
+        }
+    }
+
+    private void HighlightDiagonal2()
+    {
+        for (int row = 0, col = 2; row < 3; row++, col--)
+        {
+            //TODO: change this
+            ImageButton square = GetSquare(row, col);
+            square.ImageUrl = "Images/Red" + board.GetSquare(row, col) + ".jpg";
+        }
+    }
+
+    private void HighlightRow(int row)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            //TODO: change this
+            ImageButton square = GetSquare(row, col);
+            square.ImageUrl = "Images/Red" + board.GetSquare(row, col) + ".jpg";
+        }
+    }
+
+    private void UpdateForm()
+    {
+
+        // it's not a winner
+        for (int r = 0; r < 3; r++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                //TODO: change this
+                ImageButton square = GetSquare(r, c);
+                if (board.GetSquare(r, c) == Board.empty)
+                    square.ImageUrl = "Images/blank.jpg";
+                else if (board.GetSquare(r, c) == Board.computerSymbol)
+                {
+                    square.ImageUrl = "Images/" + Board.computerSymbol + ".jpg";
+                }
+                else
+                    square.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+                    //square.ImageUrl = "Images/" + board.GetSquare(r, c) + ".jpg";
+
+                //if (board.GetSquare(r, c) == Board.empty)
+                //    square.Enabled = true;
+                //else
+                //    square.Enabled = false;
+            }
+        }
+    }
+
+    private void DisableForm()
+    {
+        for (int r = 0; r < 3; r++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                //TODO: change this
+                ImageButton square = GetSquare(r, c);
+                square.Enabled = false;
+            }
+        }
+    }
+
+    private bool HighlightWinner(string player)
+    {
+        // rows
+        for (int row = 0; row < 3; row++)
+        {
+            //TODO: change this
+            if (board.IsRowWinner(row))
+            {
+                HighlightRow(row);
+                DisableForm();
+                results.Text = (player + " wins!");
+                return true;
+            }
+        }
+        // columns
+        for (int column = 0; column < 3; column++)
+        {
+            if (board.IsColumnWinner(column))
+            {
+                HighlightColumn(column);
+                DisableForm();
+                results.Text = (player + " wins!");
+                return true;
+            }
+        }
+        // diagonals
+        for (int row = 0; row < 3; row++)
+        {
+            if (board.IsDiagonal1Winner())
+            {
+                HighlightDiagonal1();
+                DisableForm();
+                results.Text = (player + " wins!");
+                return true;
+            }
+        }
+        for (int row = 0, col = 2; row < 3; row++, col--)
+        {
+            if (board.IsDiagonal2Winner())
+            {
+                HighlightDiagonal2();
+                DisableForm();
+                results.Text = (player + " wins!");
+                return true;
+            }
+        }
+        // full
+
+        //TODO: change this
+        return false;
+
+    }
+    protected void Image1_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+
+        if (board.IsWinner() != true || board.IsFull() != true || board.IsTie() != true)
+            UpdateForm();
+        
+    }
+    protected void Image2_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image3_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image4_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image5_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image6_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image7_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image8_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }
+    protected void Image9_Click(object sender, ImageClickEventArgs e)
+    {
+        int row;
+        int column;
+        ImageButton square = (ImageButton)sender;
+
+        GetRowAndColumn(square, out row, out column);        
+
+        board.SetSquare(row, column, Board.userSymbol);
+        UpdateForm();
+
+    }  
+
+    /*protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+            board.MakeComputerMove();
+
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if (board.GetSquare(row, col) == Board.computerSymbol)
+                {
+                    ImageButton img = GetSquare(row, col);
+                    img.ImageUrl = "Images/" + Board.computerSymbol + ".jpg";
+                }
+                else if (board.GetSquare(row, col) == Board.userSymbol)
+                {
+                    ImageButton img = GetSquare(row, col);
+                    img.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+                }
+                else
+                {
+                    ImageButton img = GetSquare(row, col);
+                    img.ImageUrl = "Images/blank.jpg";
+                }                   
+            }
+        }
+       
+        if (board.IsAnyRowWinner() == true || board.IsAnyColumnWinner() == true || board.IsAnyDiagonalWinner() == true)
+        {
+            if (board.IsDiagonal1Winner())
+            {
+                HighlightDiagonal1();
+            }
+            else if (board.IsDiagonal2Winner())
+            {
+                HighlightDiagonal2();
+            }
+            else
+            {
+                for (int r = 0; r < 3; r++)
+                {
+                    for (int c = 0; c < 3; c++)
+                    {
+                        if (board.IsRowWinner(r) == true)
+                            HighlightRow(r);
+                        else if (board.IsColumnWinner(c) == true)
+                            HighlightColumn(c);
+                    }
+                }
+            }
+        }
+    }
+    
+
+
+    protected void Image1_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);                      
+    }
+
+    protected void Image2_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == Board.userSymbol && board.GetSquare(row, column) != Board.computerSymbol)
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+       
+    }
+    protected void Image3_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+       
+    }
+    protected void Image4_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+       
+    }
+    protected void Image5_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+       
+    }
+    protected void Image6_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+       
+    }
+    protected void Image7_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+        
+    }
+    protected void Image8_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+        
+    }
+    protected void Image9_Click(object sender, ImageClickEventArgs e)
+    {
+        int row, column;
+        ImageButton my_imgbtn = (ImageButton)sender;
+        GetRowAndColumn(my_imgbtn, out row, out column);
+        board.SetSquare(row, column, Board.userSymbol);
+        if (board.GetSquare(row, column) == "X" || board.GetSquare(row, column) == "")
+        {
+            my_imgbtn.ImageUrl = "Images/" + Board.userSymbol + ".jpg";
+        }
+       
+    }*/
+}
